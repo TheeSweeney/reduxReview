40c9999328f46418647aa675939769fc7348ae9f
Brian Sweeney <bsweeney930@gmail.com>
2017-04-13 15:17:47 -0400

    added notes, made bookList a container - connected to redux test data rendering on page

--- src/reducers/reducerBooks.js
+++ src/reducers/reducerBooks.js
@@ -1,8 +1,8 @@
 export default function () {
-  return {
+  return [
     {title: "The Count of Monte Cristo"},
     {title: "Blood Meridian"},
     {title: "The Restaurant at the End of the Universe"},
     {title: "In the Heart of the Sea"}
-  }
+  ]
 }
